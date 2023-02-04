4b28c0298631faa582b9fc32ea28d426ca32280f
Benji <97198967+BenPI88@users.noreply.github.com>
2023-02-04 08:02:17 -0600

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,16 @@
 import os
 os.system("clear")
-print("(1) Ubuntu Install")
+print("(1) Ubuntu/Debian Install")
+print("(2) Fedora Install")
 print("More OSses Coming Soon!")
 option = input()
 if option == "1":
   os.system("sudo apt install curl ca-certificates -y")
   os.system("curl https://repo.waydro.id | sudo bash")
   os.system("sudo apt install waydroid -y")
+  os.system("sudo systemctl enable --now waydroid-container")
   print("\nCongrats! Waydroid is now installed on your computer!\nIf you have any issues with waydroid, check out their website over at https://waydro.id")
+if option == "2":
+  os.system("sudo dnf install waydroid")
+  os.system("sudo systemctl enable --now waydroid-container")
+  print("\nCongrats! Waydroid is now installed on your computer!\nIf you have any issues with waydroid, check out their website over at https://waydro.id\n\nSystem OTA: https://ota.waydro.id/system\nVendor OTA: https://ota.waydro.id/vendor")
